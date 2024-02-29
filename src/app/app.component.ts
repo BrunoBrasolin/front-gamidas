b9902118e315d0c2954cde9acb1b92d61910952b
Bruno Brasolin <brunobrasolinc@gmail.com>
2024-02-28 22:57:11 -0300

    Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatListModule, MatButtonModule, CommonModule],
+  imports: [MatListModule, MatButtonModule, CommonModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
