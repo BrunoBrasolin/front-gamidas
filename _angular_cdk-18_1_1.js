a246b1076216e4582cb0c8b90516f91e69056ccf
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2024-08-17 02:25:43 -0300

    Auto-generated commit

--- _angular_cdk-18_1_1.js
+++
@@ -1,1 +0,0 @@
-import{Version as o}from"@angular/core";var e=new o("18.1.1");export{e as VERSION};
---
+++ main-YURN7GXU.js
@@ -0,0 +1,1 @@
+import{a as r}from"./chunk-QEUOB3MU.js";r("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-S3SALT6K.js")).catch(o=>console.error(o));
