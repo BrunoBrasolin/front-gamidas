ca26c31a6d5397331265b2af888f55b9c8556c1c
Bruno Brasolin Carravieri <brunobrasolinc@gmail.com>
2025-01-15 22:04:17 -0300

    Revert "Change gamidas to gcli"
    This reverts commit a33519b019804cbb2fa15fdf1226ba051e976041.

---
+++ gamidas/src/new-page/files/__name@dasherize__.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gamidas-<%= dasherize(name) %>',
+  templateUrl: './<%= dasherize(name) %>.component.html',
+  styleUrls: ['./<%= dasherize(name) %>.component.scss'],
+})
+export class <%= classify(name) %>Component {}
